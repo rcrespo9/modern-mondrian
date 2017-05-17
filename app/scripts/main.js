0be9f4add610201de64750c7ee45cc966ecb47d2
rudycrespo <rudynoelcrespo@gmail.com>
2017-05-17 15:36:32 -0400

    starting on js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+(function() {
+	function moveCanvas(e) {
+		const $mondrian = document.getElementById('js-mondrian');
+		const pageXPos = e.pageX / 10;
+		const pageYPos = e.pageY / 10;;
+	}
+
+	function init() {
+		document.addEventListener('mousemove', moveCanvas);
+	}
+
+	function ready(fn) {
+	  if (document.readyState != 'loading'){
+	    fn();
+	  } else {
+	    document.addEventListener('DOMContentLoaded', init);
+	  }
+	}
+
+	ready(init);
+})();
