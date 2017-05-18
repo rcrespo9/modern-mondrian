e5276fb372b7238c564cb7a24a7ef394d627b62a
rudycrespo <rudynoelcrespo@gmail.com>
2017-05-18 15:56:00 -0400

    animating canvas on mousemove

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,21 @@
 (function() {
 	const $mondrian = document.getElementById('js-mondrian');
 
+	function moveCanvas(e) {
+		let posX = e.offsetX;
+		let posY = e.offsetY;
+
+		$mondrian.classList.remove('mondrian-canvas--inactive');
+		$mondrian.style.transform = `rotateX(${posX}deg) rotateY(${posY}deg)`;
+	}
+
+	function leavingCanvas(e) {
+		$mondrian.classList.add('mondrian-canvas--inactive');
+	}
+
 	function init() {
+		$mondrian.addEventListener('mouseover', moveCanvas);
+		$mondrian.addEventListener('mouseout', leavingCanvas);
 	}
 
 	function ready(fn) {
