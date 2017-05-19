8ada597fc40bff916d8a4102a72835ca511327d5
rudycrespo <rudynoelcrespo@gmail.com>
2017-05-19 10:12:37 -0400

    transformable mondrian

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,26 @@
 
 (function() {
 	const $mondrian = document.getElementById('js-mondrian');
+	const transformFactor = 0.1;
+
+	const scale = (n, min, max) => n * (max - min) + min;
+
+	let $mondrianRect = $mondrian.getBoundingClientRect();
 
 	function moveCanvas(e) {
-		let posX = e.offsetX;
-		let posY = e.offsetY;
+		let posX = e.pageX - $(this).offset().left;
+		let posY = e.pageY - $(this).offset().top;
+
+		let scaledX = scale(posX / $mondrianRect.width, -1, 1);
+		let scaledY = scale(posY / $mondrianRect.height,  -1, 1);
 
 		$mondrian.classList.remove('mondrian-canvas--inactive');
-		$mondrian.style.transform = `rotateX(${posX}deg) rotateY(${posY}deg)`;
+		$mondrian.style.transform = `matrix3d(
+			1, 0, ${-scaledX * transformFactor}, 0,
+			0, 1, ${-scaledY * transformFactor}, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1
+		)`;
 	}
 
 	function leavingCanvas(e) {
